7616c340fb0054af34570a4878c1752d9de655f6
Elie <deyerou.toahi@gmail.com>
2022-04-11 22:46:06 +0200

    Gestion link page 2

--- js/link-active.js
+++
@@ -1,9 +0,0 @@
-var center = document.getElementsByClassName("nav-link");
-for (let i = 0; i < center.length; i++) {
-  center[i].addEventListener("click", function (e) {
-    for (let i = 0; i < center.length; i++) {
-      center[i].classList.remove("active");
-      e.target.classList.add("active");
-    }
-  });
-}
